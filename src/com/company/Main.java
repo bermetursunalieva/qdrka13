aae7be842c801733d6f6d2c676a6515dc5825ce4
qdrka13 <bermetursunaieva@gmail.com>
2019-03-23 20:20:30 +0600

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello,world!");
+        String a = "hello,";
+        String b ="world!";
+        String c = a+b;
+        System.out.println(c);
+        final int HELLO = 5;
+        System.out.println(HELLO);
+        }
+        }
+        
